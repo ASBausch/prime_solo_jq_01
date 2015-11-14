34ccb61f4ac5bc9a614073983a4c63fbd439d81a
Amanda Bausch <manadab@me.com>
2015-11-14 15:46:48 -0600

    added remove employee button

--- soloWeekendScripts.js
+++ soloWeekendScripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-		
+	//retireveing form data in order to create new employee objects	
 	$('form#empForm').on('submit', function(event){
 		event.preventDefault();
 		firstname = $('#firstname').val();
@@ -9,6 +9,8 @@ $(document).ready(function(){
 		lastRev = $('#lastreview').val();
 		salary = $('#salary').val();
 
+	//creates and object creator in order to add detailed employee
+	//information to the dom
 		function Employee(firstname, lastname, empNum, title, lastRev, salary) {
 	   		this["First Name"] = firstname;
 	   		this["Last Name"] = lastname;
@@ -18,13 +20,25 @@ $(document).ready(function(){
 	   		this["Current Salary"] = salary;
 	   		};
 
-		
+	//creating employee objects from form input for input into dom	
 		var employee = new Employee(firstname, lastname, empNum, title, lastRev, salary);
+
+
 
 		for (var key in employee) {
 		var $p =$('<p>');
-		$p.text(key + " : " + employee[key]);
-		$('aside').append($p);
+		var $but = $('<button type="button">Remove Employee</button>');
+
+		if(key === "Current Salary"){
+			$p.text(key + ": " + "$"+ employee[key]);
+			$('aside').append($p);
+			$('p:last').append($but);
+
+			}else{
+
+			$p.text(key + ": " + employee[key]);
+			$('aside').append($p);
+			}
 		};
 
 		console.log(employee);
