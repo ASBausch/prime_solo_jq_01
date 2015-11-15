9516ca9ec97f963a9bce4630e6c896376bc074d5
Amanda Bausch <manadab@me.com>
2015-11-15 13:20:53 -0600

    commented the heck out of js file, added basic style parameters, commented html

--- soloWeekendScripts.js
+++ soloWeekendScripts.js
@@ -3,9 +3,16 @@ $(document).ready(function(){
 //writes employee information to the DOM including removal button
 	var displayEmployeeInfo= function(employee){
 
+
+//we need a container to hold each new employee information
 		var $container = $('<div>');
 
+
+//we need to loop through each employee object name/value pair 
+//and set the value to display and add them to the DOM
 		for (var i = 0; i < employee.length; i++){
+//we need variables for the point in the employee array
+//the element we will append and a remove button
 	        var elem = employee[i];
 			var $p =$('<p class="gone"> ');
 	        var $but = $('<button type="button">Remove Employee</button>');
@@ -27,6 +34,8 @@ $(document).ready(function(){
 			 		$p.text(elem.name + ": " + elem.value);
 			 		$container.append($p);
 			 } else if
+//we need to add specific classes to certain element values in 
+//order to have them display a differnt color
 			 	(elem.name == "Last Review" && elem.value == 1){
 					$p.text(elem.name + ": " + elem.value).addClass('one');
 					$container.append($p);
@@ -47,23 +56,28 @@ $(document).ready(function(){
 					$p.text(elem.name + ": " + elem.value).addClass('five');
 					$container.append($p);
 			 } else if 
+//we need to add(append) the remove button to the last printed element
 				(elem.name == "Salary"){
 	         		$p.text(elem.name + ": " + "$" + elem.value);
 	          		$container.append($p);
 	          		$container.append($but);
 	          	}
 	          };
-	          // Append the wrapper to the DOM
+// we need to append the individual containers to an element in the DOM
 
 		$container.appendTo('aside');
 
+//we need the remove button to remove it's parent container
 
 		$('button').on( "click", function() {
   			$(this).parent().remove();
 		});
-
+//the loop ends here
 	};
 
+
+//we need to take the data the is input into the form and store 
+//it in an object that can be called in our display function
 	$('form').on('submit', function(event){
 		event.preventDefault();
 		newEmployee = $(this).serializeArray();
@@ -75,6 +89,10 @@ $(document).ready(function(){
 	});
 
 
+//we need to create employee objects that can be automaticlly written
+//to the page then push them into an array that can be fed to the display
+//function
+
 var currentEmployee = [];
 	currentEmployee.push(new EmployeeCreator("Sherman", "Bausch", 1234, "Developer", 5, 10));
 	currentEmployee.push(new EmployeeCreator("Amanda", "Bausch", 2234, "Student", 1, 1));
@@ -84,7 +102,8 @@ var currentEmployee = [];
 
 	});
 
-
+//these need to be refactored to match the form fields
+//then try serializeArray
  	function EmployeeCreator(firstname, lastname, empNum, title, lastRev, salary) {
 		this.firstname = firstname;
 		this.lastname = lastname;
